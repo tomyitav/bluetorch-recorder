2380180d9950342e8c946af40d483b4644630d8d
Tom <tomyitav@gmail.com>
2017-09-22 00:38:36 +0300

    Bootstraping main

--- src/main/java/iaf/bluetorch/RecorderMain.java
+++ src/main/java/iaf/bluetorch/RecorderMain.java
@@ -1,11 +1,14 @@
 package iaf.bluetorch;
 
 import iaf.bluetorch.actors.TrackStateActor;
+import iaf.bluetorch.injector.AppInjector;
 import iaf.bluetorch.runnables.DbRequestsTask;
 import iaf.bluetorch.runnables.MessageGenerator;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import com.google.inject.Injector;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
@@ -13,6 +16,12 @@ import akka.actor.ActorSystem;
 public class RecorderMain {
 
 	public static void main(String[] args) {
+		Injector injector = AppInjector.instance().getInjector();
+		RecorderMain main = injector.getInstance(RecorderMain.class);
+		main.startRecording();
+	}
+
+	private void startRecording() {
 		ActorSystem system = ActorSystem.create("system");
 		final ActorRef trackMessageActor = system.actorOf(TrackStateActor.props(), "trackUpdates");
 		
