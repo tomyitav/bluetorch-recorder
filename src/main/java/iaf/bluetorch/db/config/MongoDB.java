d6cec4e9adc1050829a0b98d1dd703eaf007b2c7
Tom <tomyitav@gmail.com>
2017-09-23 23:49:25 +0300

    Using config for connecting to db

--- src/main/java/iaf/bluetorch/db/config/MongoDB.java
+++ src/main/java/iaf/bluetorch/db/config/MongoDB.java
@@ -20,10 +20,6 @@ import com.mongodb.ServerAddress;
 @Singleton
 public class MongoDB implements IMongoDB {
 
-	public static final String DB_HOST = "127.0.0.1";
-	public static final int DB_PORT = 27017;
-	public static final String DB_NAME = "track_history";
-
 	private Configuration config;
 	private Logger logger;
 	private Datastore datastore;
@@ -36,23 +32,28 @@ public class MongoDB implements IMongoDB {
 	}
 
 	private Datastore initializeDatastore() {
-		String dbHost = this.config.getString("database.host");
-		logger.info("InJJJJJJJEEEECCCCCTTTTTEEEEDDDDD" + dbHost);
+		String dbHost = this.config.getString("db.host");
+		int dbPort = this.config.getInt("db.port");
+		String dbName = this.config.getString("db.name");
+		int socketTimeout = this.config.getInt("db.socketTimeout");
+		int connectTimeout = this.config.getInt("db.connectTimeout");
+		int maxConnectionIdleTime = this.config.getInt("db.maxConnectionIdleTime");
+		
 		MongoClientOptions mongoOptions = MongoClientOptions.builder()
-		.socketTimeout(60000) // Wait 1m for a query to finish, https://jira.mongodb.org/browse/JAVA-1076
-		.connectTimeout(15000) // Try the initial connection for 15s, http://blog.mongolab.com/2013/10/do-you-want-a-timeout/
-		.maxConnectionIdleTime(600000) // Keep idle connections for 10m, so we discard failed connections quickly
+		.socketTimeout(socketTimeout) // Wait 1m for a query to finish, https://jira.mongodb.org/browse/JAVA-1076
+		.connectTimeout(connectTimeout) // Try the initial connection for 15s, http://blog.mongolab.com/2013/10/do-you-want-a-timeout/
+		.maxConnectionIdleTime(maxConnectionIdleTime) // Keep idle connections for 10m, so we discard failed connections quickly
 		.readPreference(ReadPreference.primaryPreferred()) // Read from the primary, if not available use a secondary
 		.build();
 	    MongoClient mongoClient;
-	    mongoClient = new MongoClient(new ServerAddress(DB_HOST, DB_PORT), mongoOptions);
+	    mongoClient = new MongoClient(new ServerAddress(dbHost, dbPort), mongoOptions);
 	
 	//    mongoClient.setWriteConcern(WriteConcern.SAFE);
 	    Datastore datastore = new Morphia().mapPackage(BasicEntity.class.getPackage().getName())
-		.createDatastore(mongoClient, DB_NAME);
+		.createDatastore(mongoClient, dbName);
 	    datastore.ensureIndexes();
 	    datastore.ensureCaps();
-	    logger.info("Connection to database '" + DB_HOST + ":" + DB_PORT + "/" + DB_NAME + "' initialized");
+	    logger.info("Connection to database '" + dbHost + ":" + dbPort + "/" + dbPort + "' initialized");
 	    return datastore;
 	}
 
